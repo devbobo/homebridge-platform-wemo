11c25afdefbca4283e078a25affc572e62e68cbb
Adrian Rudman <a.rudman@binarylight.com.au>
2015-12-23 06:01:09 +1100

    Rewrite to use wemo-client capabilities better
    Advise from Andy on how to use his module better and to use his version
    of the module.

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # homebridge-platform-wemo
 
-Belkin WeMo plugin for the awesome  [Homebridge](https://github.com/nfarina/homebridge) project.  
+Belkin WeMo Platform plugin for the awesome  [Homebridge](https://github.com/nfarina/homebridge) project.
+
+Currently supports
+- Wemo Switch
+- Wemo Insight Switch (on/off only)
+- Wemo Bulb (via Wemo Link - on/off/brightness)
 
 # Installation
 
 1. Install homebridge using: npm install -g homebridge
 2. Install this plugin using: npm install -g homebridge-platform-wemo
 3. Update your configuration file. See the sample below.
+
+# Updating
+
+Recently refactored to increase speed of operation and update on/off/brightness status more reliably.
+
+1. npm update -g homebridge-platform-wemo
 
 # Configuration
 
@@ -31,11 +42,13 @@ The module will try and find all your WeMo Devices and make them available to Ho
 
 # ToDo
 
-The code needs some work to increase the speed of status checking by persisting the WemoClient objects I am using (amateur!). I'll get to this shortly and release a new version that also depends on Timon Reinhard's core wemo-client project not my derived version.
+The code was recently updated to increase the speed of status checking by persisting the WemoClient objects properly.
 
 # Credits
 
-Credit goes to Timon Reinhard for his awesome [Wemo Client](https://github.com/timonreinhard/wemo-client) module and Andy Lindeman for the [homebridge-smartthings](https://github.com/alindeman/homebridge-smartthings) that this is work is based on.
+Credit goes to
+- Timon Reinhard for his awesome [Wemo Client](https://github.com/timonreinhard/wemo-client) module and advise 
+- Andy Lindeman for the [homebridge-smartthings](https://github.com/alindeman/homebridge-smartthings) that this is work is based on.
 
 # License
 
--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 //     }
 // ],
 "use strict";
+
 var Service, Characteristic, Accessory, uuid;
 var Wemo = require('wemo-client');
 var wemo = new Wemo();
@@ -26,8 +27,8 @@ module.exports = function (homebridge) {
 function WemoPlatform(log, config) {
 	this.log = log;
 	this.log("Wemo Platform Plugin Loaded ");
-	this.expectedAccessories = config.expected_accessories || 0;
-	this.timeout = config.timeout || 10;
+	this.expectedAccessories = config.expected_accessories || 0 ; // default to false if not specficied
+	this.timeout = config.timeout || 10; // default to 10 seconds if not specified
 }
 WemoPlatform.prototype = {
 	accessories: function (callback) {
@@ -36,12 +37,11 @@ WemoPlatform.prototype = {
 		var foundAccessories = [];
 		var self = this;
 		wemo.discover(function (device) {
-			self.log("Found: %s", device.friendlyName /* , device.deviceType, device.setupURL */ );
+			self.log("Found: %s", device.friendlyName);
 			if (device.deviceType === Wemo.DEVICE_TYPE.Bridge) { // a wemolink bridge - find bulbs
 				var client = this.client(device);
 				client.getEndDevices(function (err, enddevices) {
 					// this calls us back with an array of enddevices (bulbs)
-// 					self.log("Found: %s enddevices on %s.", enddevices.length, device.friendlyName);
 					for (var i = 0, tot = enddevices.length; i < tot; i++) {
 						self.log("Found endDevice: %s, id: %s", enddevices[i].friendlyName, enddevices[i].deviceId);
 						var accessory = new WemoAccessory(self.log, device, enddevices[i]);
@@ -53,7 +53,8 @@ WemoPlatform.prototype = {
 						}
 					}
 				});
-			} else if (device.deviceType === Wemo.DEVICE_TYPE.Switch) {
+			} else if (device.deviceType === Wemo.DEVICE_TYPE.Switch 
+					|| device.deviceType === Wemo.DEVICE_TYPE.Insight) {
 				var accessory = new WemoAccessory(self.log, device, null);
 				foundAccessories.push(accessory);
 				self.log("Discovered %s accessories of %s ", foundAccessories.length, self.expectedAccessories ? self.expectedAccessories : "an unspecified number of accessories")			
@@ -79,9 +80,57 @@ function WemoAccessory(log, device, enddevice) {
 	this.id = device.deviceId;
 	this.name = enddevice ? enddevice.friendlyName : device.friendlyName;
 	this.device = device;
-	this.enddevice = enddevice;
+	this.enddevice = enddevice;  // this is used as a flag to determine a bulb or a switch at time i.e. null = switch.
 	this.log = log;
+	this._client = wemo.client(device);	
+	this.brightness = null;
+	this._internalState = enddevice ? enddevice.internalState : null;
+	// set onState for convenience.	
+	if(enddevice) {
+		this.onState = (this._internalState['10006'].substr(0,1) === '1') ? true : false ;
+		this.log("%s is %s", this.name, this.onState, this._internalState['10006'].substr(0,1));
+	} else {
+		this.onState = device.binaryState > 0 ? true : false ;
+		this.log("%s is %s", this.name, this.onState);
+	}
+	
+	// set brightness for convenience.
+	if(enddevice) {
+		this.brightness = Math.round(this._internalState['10008'].split(':').shift() / 255 * 100 );
+		this.log("%s is %s bright", this.name, this.brightness);
+		}
+
+//	this.log(this.name , device, enddevice);
+
+ 	var self = this;
+ 	
+	this._client.on('binaryState', function(state){
+		if (!self.enddevice) { // we seem to get a spurious binaryState on lights - ignore them!
+			self.log('%s binaryState: %s', this.name, state);
+	    	self.onState = state > 0 ? true : false ;
+	    	}
+  		}.bind(this));
+  		
+ 
+  	this._client.on('statusChange', function(deviceId, capabilityId, value) {
+// 	    if (deviceId === self.deviceId) {
+//	   	  self.log('statusChange: %s', self.name, self, deviceId, capabilityId, value, this._internalState);
+	      self._statusChange(deviceId, capabilityId, value);
+// 	    	}
+			});
 }
+
+WemoAccessory.prototype._statusChange = function(deviceId, capabilityId, value) {
+   this.log('statusChange: %s', deviceId, capabilityId, value);
+   this._internalState[capabilityId] = value;
+   if (capabilityId ==='10008') {
+	   this.brightness = Math.round(this._internalState['10008'].split(':').shift() / 255 * 100 );
+	   this._internalState['10006'] = '1';  //changing wemo bulb brightness always turns them on so lets reflect this! 
+	   }
+	   
+   this.onState = (this._internalState['10006'].substr(0,1) === '1') ? true : false;
+}
+
 
 WemoAccessory.prototype.getServices = function () {
 	var services = [];
@@ -104,31 +153,28 @@ WemoAccessory.prototype.getServices = function () {
 	return services;
 };
 WemoAccessory.prototype.setOn = function (value, cb) {
-	var client = wemo.client(this.device);
+// 	var client = wemo.client(this.device);
 	this.log("setOn: % to %s", this.name, value);
-	client.setBinaryState(value ? 1 : 0);
+	this._client.setBinaryState(value ? 1 : 0);
+	this.onState = value;
 	if (cb) cb(null);
+
 }
 WemoAccessory.prototype.getOn = function (cb) {
-	// 	this.log("getOn: %s", this.name);
-	var client = wemo.client(this.device);
-	// 	this.log(client);
-	var self = this;
-	client.getBinaryState(function (err, state) {
-		self.log("getOn: %s is %s ", self.name, state);
-		if (cb) cb(null, (state > 0 ? true : false));
-	});
+	this.log("getOn: %s is %s ", this.name, this.onState);
+	if (cb) cb(null, this.onState);
 }
 WemoAccessory.prototype.setOnStatus = function (value, cb) {
-	var client = wemo.client(this.device);
-	client.setDeviceStatus(this.enddevice.deviceId, 10006, (value ? 1 : 0));
+// 	var client = wemo.client(this.device);
+	this._client.setDeviceStatus(this.enddevice.deviceId, 10006, (value ? 1 : 0));
 	this.log("setOnStatus: %s to %s", this.name, value);
 	if (cb) cb(null);
 }
 WemoAccessory.prototype.getOnStatus = function (cb) {
-	var client = wemo.client(this.device);
+// 	var client = wemo.client(this.device);
+/*
 	var self = this;
-	client.getDeviceStatus(this.enddevice.deviceId, function (err, state) {
+	this._client.getDeviceStatus(this.enddevice.deviceId, function (err, state) {
 		if(err) {
 			if(cb) {cb("unknown error getting device status (OnStatus)")}
 			}
@@ -148,17 +194,25 @@ WemoAccessory.prototype.getOnStatus = function (cb) {
 			}
 		}
 	});
+*/
+	this.log("getOnStatus: %s is %s", this.name, this.onState)
+	if(cb) cb(null, this.onState);
 }
 WemoAccessory.prototype.setBrightness = function (value, cb) {
-	var client = wemo.client(this.device);
-	client.setDeviceStatus(this.enddevice.deviceId, 10008, value*255/100 );
+// 	var client = wemo.client(this.device);
+	this._client.setDeviceStatus(this.enddevice.deviceId, 10008, value*255/100 );
 	this.log("setBrightness: %s to %s\%", this.name, value);
 	if (cb) cb(null);
 }
 WemoAccessory.prototype.getBrightness = function (cb) {
-	var client = wemo.client(this.device);
+	this.log("getBrightness: %s is %s", this.name, this.brightness)
+	if(cb) cb(null, this.brightness);
+	
+	
+// 	var client = wemo.client(this.device);
+/*
 	var self = this;
-	client.getDeviceStatus(this.enddevice.deviceId, function (err, state) {
+	this._client.getDeviceStatus(this.enddevice.deviceId, function (err, state) {
 		if(err) {
 			if(cb) {cb("unknown error getting device status (Brightness)")}
 			}
@@ -176,4 +230,5 @@ WemoAccessory.prototype.getBrightness = function (cb) {
 			}
 		}
 	});
+*/
 }
